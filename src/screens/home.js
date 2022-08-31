b81b557056935df07fe26b413b2a150f1f9a33fb
Tomas Bohn Guixeras <bohnguixeras@gmail.com>
2022-08-31 11:17:02 -0300

    Updates on UX

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import Navbar from '../components/navbar.js';
 import Add from '../components/add.js';
 import Card from '../components/card.js';
@@ -9,15 +9,37 @@ export default function Home({ navigation }) {
   return (
     <View style={{flex: 1}}>
       <Navbar navigation={navigation} onHome={true}/>
-      <ScrollView style={{flex: 1}}>
         { 
-          trackingList.length > 0 && 
-          trackingList.map( crypto => <Card key={crypto.id} crypto={crypto} trackingList={trackingList} setTrackingList={setTrackingList} />) 
+          trackingList.length > 0 ? 
+          trackingList.map( crypto => (
+            <ScrollView style={{flex: 1}}>
+              <Card key={crypto.id} crypto={crypto} trackingList={trackingList} setTrackingList={setTrackingList} />
+            </ScrollView>
+            )):
+            <View style={styles.parContainer}>
+              <Text style={styles.paragraph}>
+                Add your firt cripto to the tracking list by clicking on the + button below.
+              </Text>
+            </View>
         }
-      </ScrollView>
       <View style={{position:'absolute', bottom:0, alignSelf:'flex-end'}}>
         <Add navigation={navigation} trackingList={trackingList} setTrackingList={setTrackingList} />
       </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  parContainer:{
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  paragraph: {
+    textAlign: 'center',
+    margin: 20,
+    fontSize: 20,
+    letterSpacing: 1,
+  }
+});
